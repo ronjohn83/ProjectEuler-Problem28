a508417d31d248572622895905af6068a61ae450
Ronald Johnson <ronjohn83@hotmail.com>
2017-04-10 16:25:05 -0400

    Initial Commit

---
+++ Problem 28/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumberSpiralDiagonals());
+            Console.Read();
+        }
+
+        private static long NumberSpiralDiagonals()
+        {
+            long sum = 1;
+            int step = 2;
+            int stepSum = 1;
+            int counter = 0;
+            long total = 1001 * 1001;
+
+            while (stepSum < total)
+            {
+                while (counter < 4)
+                {
+                    stepSum += step;
+                    sum += stepSum;
+                               
+                    counter++;
+                }
+                counter = 0;
+                step += 2;
+               
+            }
+            return sum;
+        }
+    }
+}
